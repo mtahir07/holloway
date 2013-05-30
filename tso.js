046d9c1dfaec339905917777f7c53b05e62d9a63
ysim <ysim90@gmail.com>
2013-05-30 15:09:28 -0400

    temporary fix for #1

--- tso.js
+++ tso.js
@@ -7,29 +7,39 @@ casper.start(url, function() {
   this.waitFor(function check() {
     this.echo("Checking the tsoundcheck site...\n");
     return this.getCurrentUrl() == url;
+
   }, function then() {
-    this.captureSelector("concert/screenshot.png", "#CheapTickets");
-    var titleSelector = ".CheapTicketListing .eventInfo .title";
-    var title = this.getHTML(titleSelector).trim();
-    var perfSelector = ".CheapTicketListing .eventInfo .performances div";
-    var performances = this.getHTML(perfSelector).trim();
+    this.captureSelector("concerts/screenshot.png", "#CheapTickets");
+
+    var listingCount = this.evaluate(function() {
+      return document.querySelectorAll(".CheapTicketListing").length;
+    });
+    this.echo(listingCount + " listings found.");
+
     var ticketPrice = this.getHTML("#CheapTickets h2").trim();
 
-    var buyUrl = this.evaluate(function() {
-      var buyTicketsSelector = ".CheapTicketListing .eventInfo a";
-      return __utils__.findOne(buyTicketsSelector).getAttribute("href");
-    });
-    var buyUrlComplete = "http://www.tso.ca" + buyUrl;
+    for (var c = 1 ; c <= listingCount ; c++) {
+      var concertNumber = ".CheapTicketListing:nth-of-type(" + c + ") .eventInfo ";
+      var titleSelector = concertNumber + ".title";
+      var title = this.getHTML(titleSelector).trim();
+      var perfSelector = concertNumber + ".performances div";
+      var performances = this.getHTML(perfSelector).trim();
 
-    var nameFile = "concert/title.txt";
-    fs.write(nameFile, title, 'w');
+      var buyTicketsSelector = concertNumber + "a";
+      var buyUrl = this.getElementAttribute(buyTicketsSelector, "href");
+      var buyUrlComplete = "http://www.tso.ca" + buyUrl;
+      this.echo(buyUrlComplete);
 
-    var concertDetails = performances + "\n" + ticketPrice;
-    var detailsFile = "concert/details.txt";
-    fs.write(detailsFile, concertDetails, 'w');
+      var titleFile = "concerts/" + c + "/title.txt";
+      fs.write(titleFile, title, 'w');
 
-    var filename = "concert/buyUrl.txt";
-    fs.write(filename, buyUrlComplete, 'w');
+      var concertDetails = performances + "\n" + ticketPrice;
+      var detailsFile = "concerts/" + c + "/performances.txt";
+      fs.write(detailsFile, concertDetails, 'w');
+
+      var filename = "concerts/" + c + "/buyUrl.txt";
+      fs.write(filename, buyUrlComplete, 'w');
+    }
 
   }, function onTimeout() {
     this.die("URL timed out.", 1);
